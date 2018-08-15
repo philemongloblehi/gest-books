b26afa016aea6d2f9b4fa8ac17c5a911870d570a
globlehi <philemongloblehi@gmail.com>
2018-08-15 09:54:53 +0000

    Finalisation TP OC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  constructor() {
+   // Initialize Firebase
+  var config = {
+    apiKey: 'AIzaSyAY53Tvbc4mJTRYQXUmm4zJ9NOON0cOsJc',
+    authDomain: 'gestbooks-3f687.firebaseapp.com',
+    databaseURL: 'https://gestbooks-3f687.firebaseio.com',
+    projectId: 'gestbooks-3f687',
+    storageBucket: '',
+    messagingSenderId: '46956961681'
+  };
+  firebase.initializeApp(config);
+  }
 }
